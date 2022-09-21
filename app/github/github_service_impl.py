e557dfcc9635ccf99956259d9f683bd5954c2d3a
Victor Freire <victorclf@live.com>
2022-09-21 11:05:10 -0300

    Add GitHub auth with personal token

--- app/github/github_service_impl.py
+++ app/github/github_service_impl.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 
 import httpx
 from app.github.github_service import GitHubService
@@ -6,12 +7,25 @@ from app.github.github_service import GitHubService
 
 class GitHubServiceImpl(GitHubService):
     BASE_URL = 'https://api.github.com'
+    TOKEN_PATH = os.path.expanduser(os.path.join('~', '.ghstatsrc'))
     
     """ Implementation of GitHub service that connects to the public GitHub API.
     """
     def __init__(self):
         super().__init__()
-        self.client = httpx.AsyncClient(base_url=self.BASE_URL)
+        self.token = self._read_token()
+        headers = {'accept': 'application/vnd.github+json'}
+        if self.token:
+            headers['authorization'] = f'token {self.token}'
+        self.client = httpx.AsyncClient(base_url=self.BASE_URL, headers=headers)
+        
+    def _read_token(self):
+        try:
+            with open(self.TOKEN_PATH) as fin:
+                token = fin.read()
+                return token.strip()
+        except IOError:
+            return None
     
     async def get_commits(
         self,
@@ -30,6 +44,13 @@ class GitHubServiceImpl(GitHubService):
             params['since'] = since.isoformat()
         if until:
             params['until'] = until.isoformat()
+        response = await self.client.get(f'/repos/{owner}/{repo}/commits', params=params)
+        # TODO better way to log this
+        print('\nget_commits response from GitHub')
+        print(response.headers)
+        print(response.json())
+        
+        return response
 
     async def get_contributors(
         self,
@@ -39,7 +60,13 @@ class GitHubServiceImpl(GitHubService):
         per_page: int = 30,
     ):
         params = {'page': page, 'per_page': per_page}
-        return await self.client.get(f'/repos/{owner}/{repo}/contributors', params=params)
+        response = await self.client.get(f'/repos/{owner}/{repo}/contributors', params=params)
+        # TODO better way to log this
+        print('get_contributors response from GitHub')
+        print(response.headers)
+        print(response.json)
+        
+        return response
 
     async def __aexit__(self, exc_type, exc_value, traceback):
         await self.client.aclose()
