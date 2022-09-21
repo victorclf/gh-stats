8c5bcc119e2ae40be2df2caf96bf712a0cfd421f
Victor Freire <victorclf@live.com>
2022-09-21 11:02:44 -0300

    Improve API docs

--- app/routers/commits.py
+++ app/routers/commits.py
@@ -1,6 +1,6 @@
 import asyncio
 from datetime import date, datetime, timedelta
-from fastapi import APIRouter, Query, Depends
+from fastapi import APIRouter, Query, Depends, Path
 from app.dependencies import get_github_facade
 
 from app.github.github_facade import GitHubFacade
@@ -27,11 +27,12 @@ def _getDaysBetween(since: datetime, until: datetime, min_days=1, max_days=7) ->
 
 
 @router.get("/repos/{owner}/{repo}/stats/daily_commits", response_model=list[DailyCommits])
-async def get_daily_commits(owner: str,
-                            repo: str,
+async def get_daily_commits(owner: str = Path(description="The account owner of the repository. The name is not case sensitive."),
+                            repo: str = Path(description="The name of the repository. The name is not case sensitive."),
+                            author: str | None = Query(default=None, description="GitHub login of specific author"),
                             page: int = Query(default=1, description="Page number"),
-                            per_page: int = Query(default=2, ge=1, le=10, description="Authors per page"),
-                            since: datetime | None = Query(default=None, description="Start date (range will be trimmed to a maximum of 7 days after this). Timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ"),
+                            per_page: int = Query(default=2, ge=1, le=5, description="Authors per page"),
+                            since: datetime | None = Query(default=None, description="Start date (range will be trimmed to a maximum of 7 days after start date). Timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ"),
                             until: datetime | None = Query(default=None, description="End date  (range will be trimmed to a maximum of 7 days after start date). Timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ"),
                             github: GitHubFacade = Depends(get_github_facade)
                             ):
@@ -41,7 +42,7 @@ async def get_daily_commits(owner: str,
         until = datetime.now()
     days = _getDaysBetween(since, until, min_days=1, max_days=7)
 
-    contributors = await github.get_contributors(owner, repo, page, per_page)
+    contributors = [author] if author else (await github.get_contributors(owner, repo, page, per_page))
     daily_commits = []
     for contributor in contributors:
         # TO IMPROVE: use asyncio.gather to run all calls to GitHub concurrently
@@ -51,8 +52,8 @@ async def get_daily_commits(owner: str,
 
 
 @router.get("/repos/{owner}/{repo}/commits", response_model=list[Commit])
-async def get_commits(owner: str,
-                      repo: str,
+async def get_commits(owner: str = Path(description="The account owner of the repository. The name is not case sensitive."),
+                      repo: str = Path(description="The name of the repository. The name is not case sensitive."),
                       author: str | None = Query(default=None, description="GitHub login or email address by which to filter by commit author."),
                       page: int = Query(default=1, description="Page number"),
                       per_page: int = Query(default=30, ge=1, le=100, description="Items per page"),
